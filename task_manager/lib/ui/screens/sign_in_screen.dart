4c536d881832e940aff8a36b0eba89bfa565f979
RafatMeraz <razhossain201@gmail.com>
2025-07-14 23:08:18 +0600

    Add auth controller for auth state management

--- task_manager/lib/ui/screens/sign_in_screen.dart
+++ task_manager/lib/ui/screens/sign_in_screen.dart
@@ -1,8 +1,11 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_manager/data/models/user_model.dart';
 import 'package:task_manager/data/service/network_caller.dart';
 import 'package:task_manager/data/urls.dart';
+import 'package:task_manager/ui/controllers/auth_controller.dart';
 import 'package:task_manager/ui/screens/forgot_password_email_screen.dart';
 import 'package:task_manager/ui/screens/main_nav_bar_holder_screen.dart';
 import 'package:task_manager/ui/screens/sign_up_screen.dart';
@@ -142,6 +145,11 @@ class _SignInScreenState extends State<SignInScreen> {
     );
 
     if (response.isSuccess) {
+      UserModel userModel = UserModel.fromJson(response.body!['data']);
+      String token = response.body!['token'];
+
+      await AuthController.saveUserData(userModel, token);
+
       Navigator.pushNamedAndRemoveUntil(
           context, MainNavBarHolderScreen.name, (predicate) => false);
     } else {
