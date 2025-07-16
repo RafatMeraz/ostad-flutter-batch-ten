c55da889d637445d8189c44db1fe28afd2aa8453
RafatMeraz <razhossain201@gmail.com>
2025-07-16 23:22:01 +0600

    Integrate get progress tasks api

--- task_manager/lib/data/urls.dart
+++ task_manager/lib/data/urls.dart
@@ -5,4 +5,5 @@ class Urls {
   static const String loginUrl = '$_baseUrl/Login';
   static const String createNewTaskUrl = '$_baseUrl/createTask';
   static const String getNewTasksUrl = '$_baseUrl/listTaskByStatus/New';
+  static const String getProgressTasksUrl = '$_baseUrl/listTaskByStatus/Progress';
 }
