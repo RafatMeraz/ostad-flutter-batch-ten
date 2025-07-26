4274b4fad2a4808ccb48e944328710051e6b28e9
RafatMeraz <razhossain201@gmail.com>
2025-07-26 22:55:52 +0600

    Add new task list controller in task manager

--- task_manager/lib/app.dart
+++ task_manager/lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:task_manager/controller_binder.dart';
 import 'package:task_manager/ui/screens/add_new_task_screen.dart';
 import 'package:task_manager/ui/screens/change_password_screen.dart';
 import 'package:task_manager/ui/screens/forgot_password_email_screen.dart';
@@ -63,6 +64,7 @@ class TaskManagerApp extends StatelessWidget {
         AddNewTaskScreen.name: (context) => AddNewTaskScreen(),
         UpdateProfileScreen.name: (context) => UpdateProfileScreen()
       },
+      initialBinding: ControllerBinder(),
     );
   }
 }
--- task_manager/lib/ui/screens/sign_in_screen.dart
+++ task_manager/lib/ui/screens/sign_in_screen.dart
@@ -23,7 +23,7 @@ class _SignInScreenState extends State<SignInScreen> {
   final TextEditingController _emailTEController = TextEditingController();
   final TextEditingController _passwordTEController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final SignInController _signInController = SignInController();
+  final SignInController _signInController = Get.find<SignInController>();
 
   @override
   Widget build(BuildContext context) {
@@ -69,8 +69,7 @@ class _SignInScreenState extends State<SignInScreen> {
                     },
                   ),
                   const SizedBox(height: 16),
-                  GetBuilder(
-                    init: _signInController,
+                  GetBuilder<SignInController>(
                     builder: (controller) {
                       return Visibility(
                         visible: controller.inProgress == false,
