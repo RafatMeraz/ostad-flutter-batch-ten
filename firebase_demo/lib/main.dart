322aee87a2d81396bebb90d3766de3e016b1056a
RafatMeraz <razhossain201@gmail.com>
2025-08-16 22:54:23 +0600

    Add fcm service

--- firebase_demo/lib/main.dart
+++ firebase_demo/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_demo/fcm_service.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  final FCMService fcmService = FCMService();
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await fcmService.initialize();
+  print(await fcmService.getFcmToken());
+  await fcmService.onTokenRefresh();
 
   runApp(const LiveScoreApp());
 }
