e3e796398990d3e01e82bdf4b3ace6e217ae48e4
RafatMeraz <razhossain201@gmail.com>
2025-10-06 22:54:51 +0600

    Integrate category list api

--- e_commerce/lib/app/urls.dart
+++ e_commerce/lib/app/urls.dart
@@ -5,4 +5,7 @@ class Urls {
   static const String verifyOtpUrl = '$_baseUrl/auth/verify-otp';
   static const String loginUrl = '$_baseUrl/auth/login';
   static const String homeSlidersUrl = '$_baseUrl/slides';
+
+  static String categoryList(int pageNo, int pageSize) =>
+      '$_baseUrl/categories?count=$pageSize&page=$pageNo';
 }
--- task_manager/README.md
+++
@@ -1,6 +0,0 @@
-# task_manager
-
-Task manager project for Ostad batch ten
-
-## Version
-Flutter version is 3.29.0
